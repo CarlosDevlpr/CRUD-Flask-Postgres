e82fb5e9cf155c17fa2ed44aef7461f23594f741
CarlosDevlpr <carlosdiamantino13@gmail.com>
2022-07-27 23:11:44 -0300

    Adicionando banco de dados

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+db = SQLAlchemy()
+cors = CORS(app)
+
+db.init_app(app)
+Migrate(app, db)
+
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['CORS_HEADERS'] = os.getenv('CORS_HEADERS')
 
 from src.routes.user import *
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db)
